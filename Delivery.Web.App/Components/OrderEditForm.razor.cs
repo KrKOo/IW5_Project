f42e1a90ead4e4a45e14f714b993925df2b91f43
PeterCellar <petocell@gmail.com>
2022-12-10 20:43:21 +0100

    created Order forms

---
+++ Delivery.Web.App/Components/OrderEditForm.razor.cs
@@ -0,0 +1,60 @@
+﻿using Delivery.Common.Models.Order;
+using Delivery.Web.BL.Facades;
+using Microsoft.AspNetCore.Components;
+
+namespace Delivery.Web.App
+{
+    public partial class OrderEditForm
+    {
+        [Inject]
+        public OrderFacade orderFacade { get; set; } = null!;
+
+        [Parameter]
+        public Guid Id { get; init; }
+
+        [Parameter]
+        public EventCallback OnModification { get; set; }
+
+        public OrderDetailModel Data { get; set; } = GetNewOrderModel();
+
+        protected override async Task OnInitializedAsync()
+        {
+            if(Id != Guid.Empty)
+            {
+                Data = await orderFacade.GetByIdAsync(Id);
+            }
+
+            await base.OnInitializedAsync();
+        }
+
+        public async Task Save()
+        {
+            //await orderFacade.SaveAsync(createModel);
+            Data = GetNewOrderModel();
+            await NotifyOnModification();
+        }
+
+        public async Task Delete()
+        {
+            await orderFacade.DeleteAsync(Id);
+            await NotifyOnModification();
+        }
+
+        private async Task NotifyOnModification()
+        {
+            if (OnModification.HasDelegate)
+            {
+                await OnModification.InvokeAsync(null);
+            }
+        }
+
+        private static OrderDetailModel GetNewOrderModel()
+            => new()
+            {
+                Address = String.Empty,
+                DeliveryTime = TimeSpan.MinValue,
+                Note = String.Empty,
+                State = Common.Enums.OrderState.Created
+            };
+    }
+}
---
+++ Delivery.Web.App/Pages/OrderListPage.razor.cs
@@ -0,0 +1,25 @@
+﻿using Delivery.Common.Models.Order;
+using Delivery.Web.BL.Facades;
+using Microsoft.AspNetCore.Components;
+
+namespace Delivery.Web.App.Pages
+{
+    public partial class OrderListPage
+    {
+        [Inject]
+        private OrderFacade orderFacade { get; set; } = null!;
+
+        private ICollection<OrderListModel> Orders { get; set; } = new List<OrderListModel>();
+
+        protected override async Task OnInitializedAsync()
+        {
+            await LoadData();
+            await base.OnInitializedAsync();
+        }
+
+        private async Task LoadData()
+        {
+            Orders = await orderFacade.GetAllAsync();
+        }
+    }
+}
